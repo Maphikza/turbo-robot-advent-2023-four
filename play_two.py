41e0c0d0bd0b617b42984f3dd27eb881e7393389
Maphikza <stapisi@protonmail.com>
2023-12-06 14:20:59 +0200

    optimizing code for faster processing.

--- play_two.py
+++ play_two.py
@@ -1,8 +1,7 @@
-from collections import deque
 import time
 
 def count_matches(winning_numbers, your_numbers):
-    return len(winning_numbers.intersection(your_numbers))
+    return len(winning_numbers & your_numbers)
 
 # Start timing
 start_time = time.time()
@@ -10,32 +9,27 @@ start_time = time.time()
 with open("lotto.txt", "r") as file:
     lotto_cards = [line.strip() for line in file if line.strip()]
 
-total_cards = len(lotto_cards)  # Start with the original set of cards
-queue = deque()
+total_cards = 0
+card_copies = [1] * len(lotto_cards)  # Start with one copy of each card
 
-# Add initial set of cards to the queue
-for card_index in range(len(lotto_cards)):
-    queue.append((card_index, lotto_cards[card_index]))
-
-while queue:
-    card_index, card = queue.popleft()
+for index, card in enumerate(lotto_cards):
     card_parts = card.split(":")[1].split("|")
     winning_numbers = set(card_parts[0].split())
     your_numbers = set(card_parts[1].split())
 
     match_count = count_matches(winning_numbers, your_numbers)
+    total_cards += card_copies[index]
 
-    # Generate copies of the next cards in sequence based on match count
     for i in range(1, match_count + 1):
-        next_card_index = card_index + i
-        if next_card_index < len(lotto_cards):
-            queue.append((next_card_index, lotto_cards[next_card_index]))
-            total_cards += 1  # Count each new copy
+        next_index = index + i
+        if next_index < len(lotto_cards):
+            card_copies[next_index] += card_copies[index]
 
 # End timing
 end_time = time.time()
 
 print("Total cards:", total_cards)
 print("Time elapsed:", end_time - start_time)
+
 
 
